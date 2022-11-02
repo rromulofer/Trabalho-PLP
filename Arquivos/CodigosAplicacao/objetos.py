e2879292418df4394b541b8a27f99eba474e5d06
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-11-02 19:42:24 -0300

    update

--- Arquivos/CodigosAplicacao/objetos.py
+++ Arquivos/CodigosAplicacao/objetos.py
@@ -4,25 +4,30 @@
 # Ciência da Computação - UENF
 # Disciplina: PLP
 
+# Definindo a Classe pessoa
 class Pessoa:
+    # Construtor
     def __init__(self, nome: str, idade: int, altura: float):
         self.nome = nome
         self.idade = idade
         self.altura = altura
 
+    # Definindo o metodo dizer_ola()
     def dizer_ola(self):
         print(f'Olá, meu nome é {self.nome}. Tenho {self.idade} '
               f'anos e minha altura é {self.altura}m.')
 
+    # Definindo o metodo cozinhar()
     def cozinhar(self, receita: str):
         print(f'Estou cozinhando um(a): {receita}')
 
+    # Definindo o metodo andar()
     def andar(self, distancia: float):
         print(f'Saí para andar. Volto quando completar {distancia} metros')
 
 
 # Instancia um objeto da Classe "Pessoa"
-pessoa = Pessoa(nome='Rômulo', idade=22, altura=1.77)
+pessoa = Pessoa(nome='Romulo', idade=22, altura=1.77)
 
 # Chama os métodos de "Pessoa"
 pessoa.dizer_ola()
--- Arquivos/CodigosAplicacao/quicksort2.py
+++ Arquivos/CodigosAplicacao/quicksort2.py
@@ -4,66 +4,68 @@
 # Ciencia da Computacao - UENF
 # Disciplina: PLP
 
-# Python program for implementation of Quicksort Sort
+# Programa Python para implementacao do Quicksort Sort
 
-# This implementation utilizes pivot as the last element in the nums list
-# It has a pointer to keep track of the elements smaller than the pivot
-# At the very end of partition() function, the pointer is swapped with the pivot
-# to come up with a "sorted" nums relative to the pivot
+# Esta implementacao utiliza o pivo como o ultimo elemento na
+# lista
+# Possui um ponteiro para acompanhar os elementos menores que o
+# pivo
+# No final da funcao partition(), o ponteiro e trocado
+# pelo pivo para chegar a um numero "ordenado" relativo ao pivo
 
 
-# Function to find the partition position
+# Funcao para encontrar a posicao da particao
 def partition(array, low, high):
 
-    # choose the rightmost element as pivot
+    # Escolhe o elemento mais a direita como pivo
     pivot = array[high]
 
-    # pointer for greater element
+    # Ponteiro para o elemento maior
     i = low - 1
 
-    # traverse through all elements
-    # compare each element with pivot
+    # Percorre todos os elementos
+    # Compara cada elemento com pivo
     for j in range(low, high):
         if array[j] <= pivot:
 
-            # If element smaller than pivot is found
-            # swap it with the greater element pointed by i
+            # Se o elemento menor que o pivo for encontrado
+            # troca com o elemento maior apontado por i
             i = i + 1
 
-            # Swapping element at i with element at j
+            # Trocando elemento em i com elemento em j
             (array[i], array[j]) = (array[j], array[i])
 
-    # Swap the pivot element with the greater element specified by i
+    # Troca o elemento pivo pelo elemento maior especificado por i
     (array[i + 1], array[high]) = (array[high], array[i + 1])
 
-    # Return the position from where partition is done
+    # Retorna a posicao de onde a particao e feita
     return i + 1
 
-# function to perform quicksort
+# Funcao para executar quicksort
 
 
 def quickSort(array, low, high):
     if low < high:
 
-        # Find pivot element such that
-        # element smaller than pivot are on the left
-        # element greater than pivot are on the right
+        # Encontra o elemento pivo tal que
+        # elemento menor que pivo esta a esquerda
+        # elemento maior que pivo esta a direita
         pi = partition(array, low, high)
 
-        # Recursive call on the left of pivot
+        # Chamada recursiva a esquerda do pivo
         quickSort(array, low, pi - 1)
 
-        # Recursive call on the right of pivot
+        # Chamada recursiva a direita do pivo
         quickSort(array, pi + 1, high)
 
 
 data = [1, 7, 4, 1, 10, 9, -2]
-print("Unsorted Array")
+print("Array nao ordenado")
 print(data)
 
 size = len(data)
 
 quickSort(data, 0, size - 1)
 
-print('Sorted Array in Ascending Order:')
+print('Array ordenado em ordem crescente:')
 print(data)
